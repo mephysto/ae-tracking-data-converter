0eacd955c6737dbb6b4bc8c7d2dec0a43e6c0bd7
Maurice Melchers <maurice.melchers@gmail.com>
2018-10-03 15:25:09 +0800

    initial styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,15 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Convert AE tracking data to a usable JSON format</h1>
+    <div className="tracking-layout">
+      <div className="head">
+        <button>Prefill with demo content</button>
+        <button>Download JSON</button>
+      </div>
+      <textarea id="csv" placeholder="Paste in your CSV data here" />
+      <textarea id="json" placeholder="Your processed JSON will be here" />
+    </div>
   </Layout>
 )
 
